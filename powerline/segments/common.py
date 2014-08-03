0f8d82c833d80cfc5119ef629b909b19d8bbd522
Kovid Goyal <kovid@kovidgoyal.net>
2014-08-03 14:48:16 +0530

    Switch weather segment to using openweathermap.org
    The Yahoo! Weather API was no longer working and is too byzantine
    anyway.

--- powerline/segments/common.py
+++ powerline/segments/common.py
@@ -234,84 +234,23 @@ Divider highlight group used: ``background:divider``.
 
 
 # Weather condition code descriptions available at
-# http://developer.yahoo.com/weather/#codes
-weather_conditions_codes = (
-	('tornado',                 'stormy'),  # 0
-	('tropical_storm',          'stormy'),  # 1
-	('hurricane',               'stormy'),  # 2
-	('severe_thunderstorms',    'stormy'),  # 3
-	('thunderstorms',           'stormy'),  # 4
-	('mixed_rain_and_snow',     'rainy' ),  # 5
-	('mixed_rain_and_sleet',    'rainy' ),  # 6
-	('mixed_snow_and_sleet',    'snowy' ),  # 7
-	('freezing_drizzle',        'rainy' ),  # 8
-	('drizzle',                 'rainy' ),  # 9
-	('freezing_rain',           'rainy' ),  # 10
-	('showers',                 'rainy' ),  # 11
-	('showers',                 'rainy' ),  # 12
-	('snow_flurries',           'snowy' ),  # 13
-	('light_snow_showers',      'snowy' ),  # 14
-	('blowing_snow',            'snowy' ),  # 15
-	('snow',                    'snowy' ),  # 16
-	('hail',                    'snowy' ),  # 17
-	('sleet',                   'snowy' ),  # 18
-	('dust',                    'foggy' ),  # 19
-	('fog',                     'foggy' ),  # 20
-	('haze',                    'foggy' ),  # 21
-	('smoky',                   'foggy' ),  # 22
-	('blustery',                'foggy' ),  # 23
-	('windy',                           ),  # 24
-	('cold',                    'day'   ),  # 25
-	('clouds',                  'cloudy'),  # 26
-	('mostly_cloudy_night',     'cloudy'),  # 27
-	('mostly_cloudy_day',       'cloudy'),  # 28
-	('partly_cloudy_night',     'cloudy'),  # 29
-	('partly_cloudy_day',       'cloudy'),  # 30
-	('clear_night',             'night' ),  # 31
-	('sun',                     'sunny' ),  # 32
-	('fair_night',              'night' ),  # 33
-	('fair_day',                'day'   ),  # 34
-	('mixed_rain_and_hail',     'rainy' ),  # 35
-	('hot',                     'sunny' ),  # 36
-	('isolated_thunderstorms',  'stormy'),  # 37
-	('scattered_thunderstorms', 'stormy'),  # 38
-	('scattered_thunderstorms', 'stormy'),  # 39
-	('scattered_showers',       'rainy' ),  # 40
-	('heavy_snow',              'snowy' ),  # 41
-	('scattered_snow_showers',  'snowy' ),  # 42
-	('heavy_snow',              'snowy' ),  # 43
-	('partly_cloudy',           'cloudy'),  # 44
-	('thundershowers',          'rainy' ),  # 45
-	('snow_showers',            'snowy' ),  # 46
-	('isolated_thundershowers', 'rainy' ),  # 47
-)
-# ('day',    (25, 34)),
-# ('rainy',  (5, 6, 8, 9, 10, 11, 12, 35, 40, 45, 47)),
-# ('cloudy', (26, 27, 28, 29, 30, 44)),
-# ('snowy',  (7, 13, 14, 15, 16, 17, 18, 41, 42, 43, 46)),
-# ('stormy', (0, 1, 2, 3, 4, 37, 38, 39)),
-# ('foggy',  (19, 20, 21, 22, 23)),
-# ('sunny',  (32, 36)),
-# ('night',  (31, 33))):
+# http://openweathermap.org/weather-conditions
 weather_conditions_icons = {
-	'day':           '〇',
-	'blustery':      '⚑',
-	'rainy':         '☔',
-	'cloudy':        '☁',
-	'snowy':         '❅',
-	'stormy':        '☈',
-	'foggy':         '〰',
-	'sunny':         '☼',
-	'night':         '☾',
-	'windy':         '☴',
-	'not_available': '�',
-	'unknown':       '⚠',
+	'01d': '〇', '01n': '☾',
+	'02d': '☁', '02n': '☁',
+	'03d': '☁', '03n': '☁',
+	'04d': '☁', '04n': '☁',
+	'09d': '☔', '09n': '☔',
+	'10d': '☔', '10n': '☔',
+	'11d': '☈', '11n': '☈',
+	'13d': '❅', '13n': '❅',
+	'50d': '〰', '50n': '〰',
 }
 
 temp_conversions = {
-	'C': lambda temp: temp,
+	'K': lambda temp: temp,
 	'F': lambda temp: (temp * 9 / 5) + 32,
-	'K': lambda temp: temp + 273.15,
+	'C': lambda temp: temp - 273.15,
 }
 
 # Note: there are also unicode characters for units: ℃, ℉ and  K
@@ -340,60 +279,30 @@ class WeatherSegment(ThreadedSegment):
 			if not self.location:
 				location_data = json.loads(urllib_read('http://freegeoip.net/json/'))
 				self.location = ','.join([location_data['city'],
-											location_data['region_code'],
 											location_data['country_code']])
-			query_data = {
-				'q':
-				'use "http://github.com/yql/yql-tables/raw/master/weather/weather.bylocation.xml" as we;'
-				'select * from we where location="{0}" and unit="c"'.format(self.location).encode('utf-8'),
-				'format': 'json',
-			}
-			self.url = 'http://query.yahooapis.com/v1/public/yql?' + urllib_urlencode(query_data)
+			self.url = 'http://api.openweathermap.org/data/2.5/weather?' + urllib_urlencode({
+				'q': self.location})
 
 		raw_response = urllib_read(self.url)
 		if not raw_response:
 			self.error('Failed to get response')
 			return self.update_value
 		response = json.loads(raw_response)
-		condition = response['query']['results']['weather']['rss']['channel']['item']['condition']
-		condition_code = int(condition['code'])
-		temp = float(condition['temp'])
+		temp = float(response['main']['temp'])
+		wic = response['weather'][0]['icon']
 
-		try:
-			icon_names = weather_conditions_codes[condition_code]
-		except IndexError:
-			if condition_code == 3200:
-				icon_names = ('not_available',)
-				self.warn('Weather is not available for location {0}', self.location)
-			else:
-				icon_names = ('unknown',)
-				self.error('Unknown condition code: {0}', condition_code)
-
-		return (temp, icon_names)
+		return (temp, wic)
 
 	def render(self, weather, icons=None, unit='C', temp_format=None, temp_coldest=-30, temp_hottest=40, **kwargs):
 		if not weather:
 			return None
 
-		temp, icon_names = weather
-
-		for icon_name in icon_names:
-			if icons:
-				if icon_name in icons:
-					icon = icons[icon_name]
-					break
-		else:
-			icon = weather_conditions_icons[icon_names[-1]]
+		temp, weather_icon = weather
+		icon = (icons or {}).get(weather_icon, weather_conditions_icons.get(weather_icon, weather_icon))
 
 		temp_format = temp_format or ('{temp:.0f}' + temp_units[unit])
 		converted_temp = temp_conversions[unit](temp)
-		if temp <= temp_coldest:
-			gradient_level = 0
-		elif temp >= temp_hottest:
-			gradient_level = 100
-		else:
-			gradient_level = (temp - temp_coldest) * 100.0 / (temp_hottest - temp_coldest)
-		groups = ['weather_condition_' + icon_name for icon_name in icon_names] + ['weather_conditions', 'weather']
+		groups = ['weather_conditions', 'weather']
 		return [
 			{
 				'contents': icon + ' ',
@@ -402,9 +311,8 @@ class WeatherSegment(ThreadedSegment):
 			},
 			{
 				'contents': temp_format.format(temp=converted_temp),
-				'highlight_group': ['weather_temp_gradient', 'weather_temp', 'weather'],
+				'highlight_group': ['weather_temp', 'weather'],
 				'divider_highlight_group': 'background:divider',
-				'gradient_level': gradient_level,
 			},
 		]
 
@@ -424,7 +332,7 @@ weather conditions.
 :param str location_query:
 	location query for your current location, e.g. ``oslo, norway``
 :param dict icons:
-	dict for overriding default icons, e.g. ``{'heavy_snow' : u'❆'}``
+	dict for overriding default icons, e.g. ``{'13d' : u'❆'}``. See http://openweathermap.org/weather-conditions for list of icon names
 :param str temp_format:
 	format string, receives ``temp`` as an argument. Should also hold unit.
 :param float temp_coldest:
